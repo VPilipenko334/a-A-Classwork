3beeec1d37dab07ad6712df1d3454c2c4ed0c3db
Veronika <veronika.pilipenko@gmail.com>
2021-06-09 16:51:56 -0400

    Working through building out the classes

--- W5D3/aa_questions/questions_database.rb
+++ W5D3/aa_questions/questions_database.rb
@@ -24,9 +24,56 @@ class Users
         Users.new(data[0])
     end
 
+    def self.find_by_name(first,last)
+        data = QuestionsDatabase.instance.execute(<<-SQL, first, last)
+            SELECT
+                *
+            FROM
+                users
+            WHERE
+                fname = ? AND lname = ? 
+        SQL
+        Users.new(data[0])
+    end
+
     def initialize(options)
         @id = options['id']
         @fname = options['fname']
         @lname = options['lname']
     end
 end
+
+class Questions 
+    def self.find_by_id(question_id)
+        data = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+            SELECT
+                *
+            FROM
+                questions
+            WHERE
+                id = ?
+        SQL
+        Questions.new(data[0])
+    end
+
+    def self.find_by_title(q_title)
+        data = QuestionsDatabase.instance.execute(<<-SQL, q_title)
+            SELECT
+                *
+            FROM
+                questions 
+            WHERE
+                title = ? 
+        SQL
+        Questions.new(data[0])
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @title = options['title']
+        @body = options['body']
+        @user_id = options['user_id']
+    end
+
+
+end 
