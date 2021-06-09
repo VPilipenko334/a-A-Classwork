8b64616789afd5ca0e28d86ff4e71f24f054ddbd
Kevin Jo <kevinhjo725@gmail.com>
2021-06-09 14:28:11 -0400

    Add tables

---
+++ W5D3/aa_questions/import_db.sql
@@ -0,0 +1,35 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+);
+
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE question_follows (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    question_id INTEGER NOT NULL,
+    parent INTEGER,
+    user_id INTEGER NOT NULL,
+    body TEXT NOT NULL,
+
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (parent) REFERENCES replies(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
