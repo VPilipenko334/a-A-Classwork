1f8aa46d15c1fddd1ed6e0d547983874aab93d38
Veronika <veronika.pilipenko@gmail.com>
2021-06-10 17:11:36 -0400

    Worked through the first part of the project

--- W5D4/Rails Project/app/models/course.rb
+++ W5D4/Rails Project/app/models/course.rb
@@ -1,2 +1,20 @@
 class Course < ApplicationRecord
+
+    has_many: :enrollments,
+        primary_key: :id, 
+        foreign_key: :course_id, 
+        class_name: :enrollments
+
+    has_many: :enrolled_students,
+        through: :enrollments,
+        source: :student
+
+    has_one: :prequisite, 
+        primary_key: :id,
+        foreign_key: :prereq_id,
+        class_name: :Course
+
+        
+
+
 end
