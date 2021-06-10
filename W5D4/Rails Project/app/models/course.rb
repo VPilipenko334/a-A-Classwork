628d0593e431d89ceb88d6476fb480a22d0bf1b4
Joseph Kim <jkjoeeekim@live.com>
2021-06-10 14:42:28 -0700

    Finish associations

--- W5D4/Rails Project/app/models/course.rb
+++ W5D4/Rails Project/app/models/course.rb
@@ -1,20 +1,21 @@
 class Course < ApplicationRecord
+  has_many :enrollments,
+           primary_key: :id,
+           foreign_key: :course_id,
+           class_name: :Enrollment
 
-    has_many: :enrollments,
-        primary_key: :id, 
-        foreign_key: :course_id, 
-        class_name: :enrollments
+  has_many :enrolled_students,
+           through: :enrollments,
+           source: :student
 
-    has_many: :enrolled_students,
-        through: :enrollments,
-        source: :student
+  belongs_to :prequisite,
+             primary_key: :id,
+             foreign_key: :prereq_id,
+             class_name: :Course,
+             optional: true
 
-    has_one: :prequisite, 
-        primary_key: :id,
-        foreign_key: :prereq_id,
-        class_name: :Course
-
-        
-
-
+  belongs_to :instructor,
+             primary_key: :id,
+             foreign_key: :instructor_id,
+             class_name: :User
 end
--- W5D4/Rails Project/app/models/user.rb
+++ W5D4/Rails Project/app/models/user.rb
@@ -1,14 +1,15 @@
 class User < ApplicationRecord
+  has_many :enrollments,
+           primary_key: :id,
+           foreign_key: :student_id,
+           class_name: :Enrollment
 
-    has_many: :enrollments,
-        primary_key: :id,
-        foreign_key: :student_id,
-        class_name: :Enrollment
+  has_many :enrolled_courses,
+           through: :enrollments,
+           source: :course
 
-    has_many: :enrolled_courses,
-        through: :enrollments,
-        source: :course 
-
-    
-
+  has_many :courses,
+           primary_key: :id,
+           foreign_key: :instructor_id,
+           class_name: :Course
 end
