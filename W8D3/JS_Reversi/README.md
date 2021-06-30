f98baae2db42ce11b0267c1b7d588527783316ce
veronika <veronika.pilipenko@gmail.com>
2021-06-30 17:27:53 -0400

    Finished creating the piece

--- W8D3/JS_Reversi/README.md
+++ W8D3/JS_Reversi/README.md
@@ -3,4 +3,3 @@
 To run tests, open `SpecRunner.html` in a chrome tab.
 
 To play the game, run `node run_game.js` in your console.
-
--- W8D3/JS_Reversi/src/piece.js
+++ W8D3/JS_Reversi/src/piece.js
@@ -2,18 +2,29 @@
  * Initializes the Piece with its color.
  */
 function Piece (color) {
+
+    this.color = color;
 }
 
 /**
  * Returns the color opposite the current piece.
  */
 Piece.prototype.oppColor = function () {
+    if(this.color === "white") {
+        return "black";
+    } else  {
+        return "white"; }
 };
 
 /**
  * Changes the piece's color to the opposite color.
  */
 Piece.prototype.flip = function () {
+    if (this.color === "white") {
+        this.color = "black";
+    } else {
+        this.color = "white"; 
+    }
 };
 
 /**
@@ -21,8 +32,12 @@ Piece.prototype.flip = function () {
  * based on its color.
  */
 Piece.prototype.toString = function () {
+
+    return this.color;
+
 };
 
+    
 // DON'T TOUCH THIS CODE
 if (typeof window === 'undefined'){
     module.exports = Piece;
