04865856abfab3417daf73c9c3cd85fae702de92
veronika <veronika.pilipenko@gmail.com>
2021-06-29 20:54:17 -0400

    myEach and myMap in JavaScript

--- W8D1/W8D1 JavaScript Project 1/phase_1_arrays.js
+++ W8D1/W8D1 JavaScript Project 1/phase_1_arrays.js
@@ -4,3 +4,22 @@ Array.prototype.uniq = function() {
     for (let i = 0; i <this.length; i++)
         if (uniqueArray.indexOf)
 }
+
+//myEach 
+Array.prototype.myEach = function(callback) {
+    for (let i=0; this.length, i++) {
+        current = this[i];
+        console.log(callback(current));
+    }
+}
+
+//blocks are like procs, procs are like callbacks
+
+//myMap
+Array.prototype.myMap = function(callback) {
+    const array = [];
+    this.myEach(el => array.push(callback(el)));
+    return array;
+}
+//callbacks are similar to a PROC
+
