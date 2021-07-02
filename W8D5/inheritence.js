043eb654bbac3254de82a493c9bed7cd973bc10c
veronika <veronika.pilipenko@gmail.com>
2021-07-02 19:35:47 -0400

    Inheritence

---
+++ W8D5/inheritence.js
@@ -0,0 +1,38 @@
+
+// `Function.prototype.inherits` using surrogate 
+
+Function.prototype.inherits1 = function (BaseClass) {
+    function Surrogate() { }
+    Surrogate.prototype = BaseClass.prototype;
+    this.prototype = new Surrogate();
+    this.prototype.constructor = this;
+};
+
+// `Function.prototype.inherits` using `Object.create`
+
+Function.prototype.inherits2 = function (BaseClass) {
+    this.prototype = Object.create(BaseClass.prototype);
+    this.prototype.constructor = this;
+};
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.bark = function () {
+    console.log(this.name + " goes woof");
+};
+
+function Doggy(name) {
+    Dog.call(this, name);
+}
+
+Doggy.inherits1(Dog);
+
+Doggy.prototype.waddle = function () {
+    console.log(this.name + " waddles!");
+};
+
+const blixa = new Doggy("Toto");
+blixa.bark();
+blixa.waddle();
---
+++ W8D5/w8D5.js
@@ -0,0 +1,63 @@
+
+// problem 1
+
+const sum = num1 => num2 => num3 => num4 => num1 + num2 + num3 + num4 + num5;
+
+
+sum(1, 2, 3, 4) === 10;
+
+
+sum(1, 2, 3, 4, 5) === 15;
+
+
+
+
+
+
+
+
+// problem 2 
+class Cat {
+    constructor(name) {
+        this.name = name;
+    }
+
+    says(sound, person) {
+        console.log(`${this.name} says ${sound} to ${person}!`);
+        return true;
+    }
+}
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBind(pavlov);
+notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
