a29e85c0fede6d808ad279bb777217d0d5e6ca5e
Veronika <veronika.pilipenko@gmail.com>
2021-07-20 17:33:40 -0700

    Finishing up the project

--- W11D2/frontend/todo_redux.jsx
+++ W11D2/frontend/todo_redux.jsx
@@ -1,8 +1,15 @@
 import React from 'react'; 
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+import { receiveTodo } from './actions/todo_actions';
+import { receiveTodos } from './actions/todo_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
-
+    const store = configureStore(); // creates the store
+    window.store = store; // lets us access the store from window
+    window.receiveTodo = receiveTodo;
+    window.receiveTodos = receiveTodos;
+    
     let content = document.getElementById('content');
     ReactDOM.render(<h1>Todos App</h1>, content);
 });
