08759c284c3957e814a87e0b44e98277d395eb23
Veronika <veronika.pilipenko@gmail.com>
2021-07-22 12:35:56 -1000

    Finished up todo_list.jsx

--- W11D2/frontend/components/todos/todo_list.jsx
+++ W11D2/frontend/components/todos/todo_list.jsx
@@ -1,8 +1,29 @@
 //presentational component
-
-
 import React from 'react';
-import toD from './todo_list_container';
+import TodoListItem from './todo_list/todo_list_item';
+import TodoForm from './todo_list/todo_form';
 
-export default () => <h3>Todo List goes here!</h3>
+const TodoList = (props) => {
+    const todos = props.todos;
+    const listItems = todos.map(todo => {
+        return <TodoListItem todo ={todo}/> 
+    })
+
+    return  (
+        <div>
+            <h1>My To-Do List:</h1>
+            <ul>
+                {listItems}
+            </ul>
+            <TodoForm receiveTodo={props.receiveTodo}/>
+        </div>
+    )
+
+}
+export default ToDoList;
+
+// export default () => <h3>Todo List goes here!</h3>
+
+//mapping out the list: 
+// {todos.map(todo -> <li key = {todo.id}> {todo.title}</li>)}
 
